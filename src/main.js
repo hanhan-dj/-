14459a63e5b12795ca289d816e38e8884877105c
hanhan <2676646215@qq.com>
2022-11-18 01:39:08 +0800

    项目初始化-调整项目初始目录

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less' // 加载全局样式
 
 createApp(App).use(store).use(router).mount('#app')
